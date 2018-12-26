489b77fc562f81bff6322d0e223e91528588d78b
Kiowa Daly <kiowa.daly@ucdconnect.ie>
2018-12-26 18:24:48 +0000

    added two button links-not working yet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+// import {homepage} from '/src/app/components/homepage';
+// import {shop} from '/src/app/components/shop';
+const routes: Routes = [
+  // { path: 'app-homepage', component: homepage},
+  // {path: 'app-shop', component: shop}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
-
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { ShopComponent } from './components/shop/shop.component';
+
 
 @NgModule({
   declarations: [
